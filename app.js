9712675a5108fe0e6a1f300b3b2967f91b2a83ec
Robert Fitzgerald <robfitzgerald@gmail.com>
2016-08-26 15:12:02 -0600

    pushed ucd assets out one level to ucd/..

--- app.js
+++ app.js
@@ -7,8 +7,14 @@
 	var express = require('express')
 		, app = express()
 
-	app.use('/csci5408', require('go-graph-presentation'))
-	app.use('/csci7551', require('csci7551-course-website'))
+	app.use('/ucd', require('./routes/ucd'))
+	// app.use('/csci5408', require('go-graph-presentation'))
+	// app.use('/csci7551', require('csci7551-course-website'))
+	// app.use('/', (req, res, next) => {
+	// 	res.sendFile(__dirname + '/public/html/index.html', (err) => {
+	// 		if (err) next(err)
+	// 	})
+	// })
 	app.use('/', (req, res, next) => {
 		res.sendFile(__dirname + '/public/html/index.html', (err) => {
 			if (err) next(err)
---
+++ routes/ucd.js
@@ -0,0 +1,17 @@
+'use strict';
+{
+	var express = require('express')
+		, router = express.Router()
+		, path = require('path')
+
+	router
+		.use('/csci5408', require('go-graph-presentation'))
+		.use('/csci7551', require('csci7551-course-website'))
+		.use('/', (req, res, next) => {
+			res.sendFile(path.dirname(__dirname) + '/public/html/ucd.html', (err) => {
+				if (err) next(err)
+			})
+		})
+
+	module.exports = router
+}
