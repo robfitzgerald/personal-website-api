cd6d1ea5a07ce5a20ad090ee69907e29874c8338
Rob Fitzgerald <robfitzgerald@gmail.com>
2017-01-17 16:53:44 -0700

    added csci5593 course website dependency and links

--- routes/ucd.js
+++ routes/ucd.js
@@ -7,6 +7,7 @@
 	router
 		.use('/csci5408', require('go-graph-presentation'))
 		.use('/csci7551', require('csci7551-course-website'))
+		.use('/csci5593', require('csci5593-course-website'))
 		.use('/', (req, res, next) => {
 			res.sendFile(path.dirname(__dirname) + '/public/html/ucd.html', (err) => {
 				if (err) next(err)
